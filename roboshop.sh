8c3484b0847673122d0db00a315d313478eed62d
nari.naresh442@gmail.com <nari.naresh442@gmail.com>
2025-09-28 04:33:14 +0530

    robo1

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0a211cbb2d25869bb"
 DOMAIN_NAME="ndaws86s.fun"
-
+ZONE_ID="Z02356993HYLCXH1SICVV"
 for instance in $@ 
 do
         INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)        
